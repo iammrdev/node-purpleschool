593a8b2a87259db590c4c35e7131fed505f155e6
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-10-30 22:36:06 +0300

    bot: drop prototype

--- telegram-bot/src/controllers/back.ts
+++
@@ -1,9 +0,0 @@
-import { MyContext } from '../types';
-import { mainMenu } from './start';
-
-export const back = (ctx: MyContext) =>
-    ctx.reply(`✅ Ты находишься в меню`, {
-        disable_web_page_preview: true,
-        parse_mode: 'HTML',
-        ...mainMenu,
-    });
