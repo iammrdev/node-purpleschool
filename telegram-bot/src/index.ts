00da6f66cbb2bedb6324897a985cd56c03cbafe5
Vitalii Iashchuk <iashchuk@hotmail.com>
2022-10-30 20:24:29 +0300

    bot: connect to postgresql

--- telegram-bot/src/index.ts
+++ telegram-bot/src/index.ts
@@ -1,9 +1,12 @@
 import 'dotenv/config';
-
+import { PrismaClient } from '@prisma/client';
 import { setupBot } from './bot';
 
 (async function () {
     try {
+        const prisma = new PrismaClient();
+        await prisma.$connect();
+
         const bot = setupBot();
         await bot.launch();
         console.log('</ Бот успешно запущен >');
